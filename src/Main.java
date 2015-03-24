c7724aef70987f0864b29883719004d24944de91
Ivan <mosk1995@gmail.com>
2015-03-24 16:44:16 +0300

    Добавлено считывание файла

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Created by Xenomorf on 24.03.2015.
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("HI");
+        File file=new File("text.txt");
+        String text=new String();
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (scanner.hasNextLine()){
+            text+=scanner.nextLine()+"\n";
+        }
+        System.out.println(text);
     }
 }
